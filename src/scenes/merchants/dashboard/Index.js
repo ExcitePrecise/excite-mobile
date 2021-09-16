5fe682610d9d4fa4169dc4cd48eab054e7576fe9
ojotuk <ojotuk14@yahoo.com>
2021-09-16 12:13:16 +0100

    added flash notification

--- src/scenes/merchants/dashboard/Index.js
+++ src/scenes/merchants/dashboard/Index.js
@@ -18,42 +18,57 @@ import { rememberToken, getValidToken } from './../../../utils/axios/token'
 import Header from './subs/Header'
 import { getProfileInfo } from '../../../apis/auth'
 import { saveMe } from '../../../slices/app.slice'
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 const navItem = [
   {
     name: 'Post a Product',
     icon: images.post_icon,
     navigate: 'ProductListing',
+    paid:false,
+    level:0,
   },
   {
     name: 'List a Service',
     icon: images.repairs_icon,
     navigate: 'ProductListing',
+    paid:false,
+    level:0,
   },
   {
     name: 'Place Banner',
     icon: images.banner,
     navigate: 'PostBanner',
+    paid:true,
+    level:2,
   },
   {
     name: 'Biz Book',
     icon: images.book,
     navigate: 'BookKeeping',
+    paid:false,
+    level:0,
   },
   {
     name: 'Influencer Marketing',
     icon: images.influencer,
     navigate: 'Influencer',
+    paid:false,
+    level:0,
   },
   {
     name: 'Social Commerce',
     icon: images.social,
     navigate: 'SocialCommerce',
+    paid:true,
+    level:3,
   },
   {
     name: 'Manage Listing',
     icon: images.top,
     navigate: 'ManageListing',
+    paid:false,
+    level:0,
   },
   // {
   //   name: 'Help Desk',
@@ -64,16 +79,22 @@ const navItem = [
     name: 'My Store',
     icon: images.services_icon,
     navigate: 'Store',
+    paid:false,
+    level:0,
   },
   {
     name: 'My Account',
     icon: images.accounts,
     navigate: 'MyAccount',
+    paid:false,
+    level:0,
   },
   {
     name: 'Subscription',
     icon: images.wallet,
     navigate: 'Subscription',
+    paid:false,
+    level:0,
   },
   // {
   //   name: 'Verify Email',
@@ -82,11 +103,38 @@ const navItem = [
   // },
 ]
 
-function DasboardRoute({navigation}) {
+function DasboardRoute({navigation, userSub}) {
+  const handleSubType=(sub)=>{
+    switch (sub) {
+      case 1:
+        return "Bronze"
+      case 2:
+        return "Silver"
+      case 3:
+        return "Gold"
+      default:
+        break;
+    }
+  }
   return (
     <View style={styles.route}>
       {navItem.map((item, index) => (
-        <TouchableOpacity style={styles.links} key={index} onPress={()=>navigation.navigate(item.navigate)}>
+        <TouchableOpacity style={styles.links} key={index} 
+        onPress={
+          ()=> {
+            if(item?.paid && item.level > userSub){
+              return(
+                showMessage({
+                  message:"Paid Module",
+                  description:`Upgrade to ${handleSubType(item.level)} package`,
+                  type:"danger",
+                  icon:"auto"
+                })
+              )
+          }
+          navigation.navigate(item.navigate)
+        }
+        }>
           <Image
             source={item.icon}
             width={20}
@@ -102,9 +150,9 @@ function DasboardRoute({navigation}) {
 
 
 //
-const Index = ({navigation,token}) => {
+const Index = ({navigation,token, userSub}) => {
 const dispatch = useDispatch();
-
+// console.log(userSub)
   // Refresh control
   const [refreshing, setRefreshing] = React.useState(false)
 
@@ -139,7 +187,7 @@ const dispatch = useDispatch();
       <View>
         {/* <Text>Dashboard</Text> */}
         <Header />
-        <DasboardRoute navigation={navigation}/>
+        <DasboardRoute navigation={navigation} userSub={userSub}/>
       </View>
     </ScrollView>
   )
@@ -148,7 +196,8 @@ const dispatch = useDispatch();
 
 const mapStateToProps = (state)=>{
   return{
-    token:state.app?.token
+    token:state.app?.token,
+    userSub:state.app?.me?.subscriptionLevel,
   }
 }
 export default connect(mapStateToProps)(Index);
